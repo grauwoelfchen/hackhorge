bbf65d3c65993dc1bfea955caa2fefa16ebfb452
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2023-02-12 08:41:26 +0900

    Add /mnt/hfsplus-disk/Users/Darwin/Applications ;)

---
+++ src/routes/mnt/hfsplus-disk/Users/Darwin/Applications/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "../../../../../../page.css";
+</script>
+
+<span class="path">/mnt/hfsplus-disk/Users/Darwin/Applications</span>
+<h1>macOS/iOS</h1>
+<div>
+  <p>The list of macOS/iOS I worked / am working on</p>
+  <ul>
+    <li><a href="https://apps.apple.com/us/app/generika/id520038123?ls=1">Generika</a> ... Barcode Leser für das iPhone mit Generika.cc</li>
+  </ul>
+</div>
