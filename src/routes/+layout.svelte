9972e83ee7de0bbda50a7f60e3e475c8172d49e4
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2023-02-12 08:42:14 +0900

    Update copyright

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,7 +15,7 @@
     <slot></slot>
   </main>
   <footer>
-    <p class="copyright">&copy; 2011-2022 Yasuhiro Яша Asaka (Yasha)</p>
+    <p class="copyright">&copy; 2011-2023 Yasuhiro Яша Asaka (Yasha)</p>
     <ul>
       <li class="code-of-ethics"><a href="/code-of-ethics.html">Code of Ethics</a></li>
       <li class="server-status">Server Status</li>
