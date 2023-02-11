dc1a5bacf4ecd47d448f53b3877cce7b71b05e2c
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2023-02-12 08:40:58 +0900

    Add /opt/android-studio

---
+++ src/routes/opt/android-studio/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../../../page.css";
+</script>
+
+<span class="path">/opt/android-studio</span>
+<h1>Android apps</h1>
+<div>
+  <p>The list of Android apps I worked on/am working on</p>
+  <ul>
+    <li><a href="https://github.com/grauwoelfchen/nowt">Nowt</a> ... An immersive RSS reader for Android</li>
+    <li><a href="https://play.google.com/store/apps/details?id=org.oddb.generika">Generika</a> ... Generika App for Android, based on the Generikacc App for iOS</li>
+  </ul>
+</div>
